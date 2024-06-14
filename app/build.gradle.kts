fff971b99ae7b69ca41f8106d82b2d459fc1d538
aminamehdiyeva <aminamehdi62@gmail.com>
2024-06-14 12:12:30 +0400

    styled pages

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,6 +76,8 @@ dependencies {
     implementation ("androidx.compose.ui:ui-text-google-fonts:1.6.7")
     implementation ("androidx.compose.material:material-icons-extended:1.6.8")
 
+    implementation ("androidx.navigation:navigation-compose:2.7.7")
+
 
 
 
