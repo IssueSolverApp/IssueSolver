6185ce65cb363bd85f7e3b74f434e2f9d2fa34bb
YuriyQukunov <kukunovyura29@gmail.com>
2024-06-24 03:58:57 -0700

    confirmOtp / ApiService, repository, useCase

--- app/src/main/java/com/issuesolver/presentation/MainActivity.kt
+++ app/src/main/java/com/issuesolver/presentation/MainActivity.kt
@@ -5,36 +5,15 @@ import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.core.view.WindowCompat
-import androidx.compose.foundation.gestures.scrollBy
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.ime
-import androidx.compose.foundation.layout.imePadding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalDensity
-import androidx.core.view.WindowCompat
-import com.issuesolver.presentation.login.daxil_ol_page.LoginPage
-import com.issuesolver.presentation.login.daxil_ol_page_email.EmailVerificationPage
-import com.issuesolver.presentation.login.daxil_ol_verification_code.VerificationCodePage
 import com.issuesolver.presentation.navigation.AppNavigation
 import com.issuesolver.ui.theme.IssueSolverTheme
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
-import kotlin.text.Typography.dagger
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
-//        WindowCompat.setDecorFitsSystemWindows(window, false)
 
         window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
 
