419b3821a39b2761816a3ed33229065c8be4d748
YuriyQukunov <kukunovyura29@gmail.com>
2024-07-09 13:23:37 -0700

    fix Request Screen

--- app/src/main/java/com/issuesolver/presentation/newrequest/RequestScreen.kt
+++ app/src/main/java/com/issuesolver/presentation/newrequest/RequestScreen.kt
@@ -1,6 +1,7 @@
 package com.issuesolver.presentation.newrequest
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,7 +10,9 @@ import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Divider
@@ -22,105 +25,219 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 
 @Composable
 fun RequestScreen() {
     val suggestions = listOf("Kotlin", "Java", "Dart", "Python")
 
-    Scaffold {padding->
+    Scaffold { padding ->
 
-        Box(Modifier
-            .fillMaxSize()
-            .statusBarsPadding()
-            .navigationBarsPadding()) {
+        Box(
+            Modifier
+                .fillMaxSize()
+                .statusBarsPadding()
+                .navigationBarsPadding()
+        ) {
+            ConstraintLayout(Modifier.fillMaxSize()) {
+                val (content, buttons) = createRefs()
 
-
-            Column() {
-                Navigation()
-
-                Divider(
-                    thickness = 0.5.dp,
-                    color = Color(0xFF2981FF),
-                    modifier = Modifier.padding(
-                        start = 20.dp,
-                        end = 20.dp,
-                        top = 20.dp,
-                        bottom = 16.dp
-                    )
-                )
-
-                AddLocation()
-
-                DropDownMenu(
-                    category = "Problemin Kateqoriyası",
-                    placeHolder = "Kateqoriya",
-                    list = suggestions
-                )
-
-                DropDownMenu(
-                    category = "Problemin yönləndiriləcəyi qurum",
-                    placeHolder = "Qurum",
-                    list = suggestions
-                )
-
-                Description()
-            }
-
-            Column(
-                modifier = Modifier
-                    .align(Alignment.BottomCenter)
-                    .imePadding()
-                    .background(Color(0xFFF0F4F9))
-
-            ) {
-                Button(
-                    onClick = { },
+                Column(
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(top = 16.dp, start = 20.dp, end = 20.dp),
-                    shape = RoundedCornerShape(50.dp),
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = Color(0xFF2981FF),
-                        disabledContainerColor = Color(0xFF9AC2FB)
-                    )
+                        .verticalScroll(rememberScrollState())
+                        .constrainAs(content) {
+                            top.linkTo(parent.top)
+                            bottom.linkTo(buttons.top)
+                            height = Dimension.fillToConstraints
+                        }
                 ) {
-                    Text(
-                        text = "Paylaş",
-                        fontWeight = FontWeight.W500,
-                        fontSize = 15.sp,
-                        color = Color.White,
-                        modifier = Modifier
-                            .align(Alignment.CenterVertically)
-                            .padding(top = 14.dp, bottom = 14.dp)
-                    )
-                }
+                    Navigation()
 
-                Button(
-                    onClick = { },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(top = 16.dp, start = 20.dp, end = 20.dp, bottom = 10.dp),
-                    shape = RoundedCornerShape(50.dp),
-                    colors = ButtonDefaults.buttonColors(
-                        containerColor = Color(0xFFFFFFFF),
-                        disabledContainerColor = Color(0xFFFFFFFF)
-                    )
-                ) {
-                    Text(
-                        text = "Sıfırla",
-                        fontWeight = FontWeight.W500,
-                        fontSize = 15.sp,
+                    Divider(
+                        thickness = 0.5.dp,
                         color = Color(0xFF2981FF),
-                        modifier = Modifier
-                            .align(Alignment.CenterVertically)
-                            .padding(top = 14.dp, bottom = 14.dp)
+                        modifier = Modifier.padding(
+                            start = 20.dp,
+                            end = 20.dp,
+                            top = 20.dp,
+                            bottom = 16.dp
+                        )
                     )
+
+                    AddLocation()
+
+                    DropDownMenu(
+                        category = "Problemin Kateqoriyası",
+                        placeHolder = "Kateqoriya",
+                        list = suggestions
+                    )
+
+                    DropDownMenu(
+                        category = "Problemin yönləndiriləcəyi qurum",
+                        placeHolder = "Qurum",
+                        list = suggestions
+                    )
+
+                    Description()
+                }
+
+                Column(
+                    modifier = Modifier
+                        .constrainAs(buttons) {
+                            bottom.linkTo(parent.bottom)
+                        }
+                        .imePadding()
+                        .background(Color(0xFFF0F4F9))
+                ) {
+                    Button(
+                        onClick = { },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(top = 8.dp, start = 20.dp, end = 20.dp),
+                        shape = RoundedCornerShape(50.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color(0xFF2981FF),
+                            disabledContainerColor = Color(0xFF9AC2FB)
+                        )
+                    ) {
+                        Text(
+                            text = "Paylaş",
+                            fontWeight = FontWeight.W500,
+                            fontSize = 15.sp,
+                            color = Color.White,
+                            modifier = Modifier
+                                .align(Alignment.CenterVertically)
+                                .padding(top = 14.dp, bottom = 14.dp)
+                        )
+                    }
+
+                    Button(
+                        onClick = { },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(top = 16.dp, start = 20.dp, end = 20.dp, bottom = 10.dp),
+                        shape = RoundedCornerShape(50.dp),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color(0xFFFFFFFF),
+                            disabledContainerColor = Color(0xFFFFFFFF)
+                        )
+                    ) {
+                        Text(
+                            text = "Sıfırla",
+                            fontWeight = FontWeight.W500,
+                            fontSize = 15.sp,
+                            color = Color(0xFF2981FF),
+                            modifier = Modifier
+                                .align(Alignment.CenterVertically)
+                                .padding(top = 14.dp, bottom = 14.dp)
+                        )
+                    }
                 }
             }
-
         }
-
-
     }
 }
+
+//
+//@Composable
+//fun RequestScreen() {
+//    val suggestions = listOf("Kotlin", "Java", "Dart", "Python")
+//
+//    Scaffold {padding->
+//
+//        Box(Modifier
+//            .fillMaxSize()
+//            .statusBarsPadding()
+//            .navigationBarsPadding()) {
+//
+//
+//            Column( modifier = Modifier
+//                .verticalScroll(rememberScrollState())
+//            ) {
+//                Navigation()
+//
+//                Divider(
+//                    thickness = 0.5.dp,
+//                    color = Color(0xFF2981FF),
+//                    modifier = Modifier.padding(
+//                        start = 20.dp,
+//                        end = 20.dp,
+//                        top = 20.dp,
+//                        bottom = 16.dp
+//                    )
+//                )
+//
+//                AddLocation()
+//
+//                DropDownMenu(
+//                    category = "Problemin Kateqoriyası",
+//                    placeHolder = "Kateqoriya",
+//                    list = suggestions
+//                )
+//
+//                DropDownMenu(
+//                    category = "Problemin yönləndiriləcəyi qurum",
+//                    placeHolder = "Qurum",
+//                    list = suggestions
+//                )
+//
+//                Description()
+//            }
+//
+//            Column(
+//                modifier = Modifier
+//                    .align(Alignment.BottomCenter)
+//                    .imePadding()
+//                    .background(Color(0xFFF0F4F9))
+//
+//            ) {
+//                Button(
+//                    onClick = { },
+//                    modifier = Modifier
+//                        .fillMaxWidth()
+//                        .padding(top = 16.dp, start = 20.dp, end = 20.dp),
+//                    shape = RoundedCornerShape(50.dp),
+//                    colors = ButtonDefaults.buttonColors(
+//                        containerColor = Color(0xFF2981FF),
+//                        disabledContainerColor = Color(0xFF9AC2FB)
+//                    )
+//                ) {
+//                    Text(
+//                        text = "Paylaş",
+//                        fontWeight = FontWeight.W500,
+//                        fontSize = 15.sp,
+//                        color = Color.White,
+//                        modifier = Modifier
+//                            .align(Alignment.CenterVertically)
+//                            .padding(top = 14.dp, bottom = 14.dp)
+//                    )
+//                }
+//
+//                Button(
+//                    onClick = { },
+//                    modifier = Modifier
+//                        .fillMaxWidth()
+//                        .padding(top = 16.dp, start = 20.dp, end = 20.dp, bottom = 10.dp),
+//                    shape = RoundedCornerShape(50.dp),
+//                    colors = ButtonDefaults.buttonColors(
+//                        containerColor = Color(0xFFFFFFFF),
+//                        disabledContainerColor = Color(0xFFFFFFFF)
+//                    )
+//                ) {
+//                    Text(
+//                        text = "Sıfırla",
+//                        fontWeight = FontWeight.W500,
+//                        fontSize = 15.sp,
+//                        color = Color(0xFF2981FF),
+//                        modifier = Modifier
+//                            .align(Alignment.CenterVertically)
+//                            .padding(top = 14.dp, bottom = 14.dp)
+//                    )
+//                }
+//            }
+//
+//        }
+//
+//    }
+//}
