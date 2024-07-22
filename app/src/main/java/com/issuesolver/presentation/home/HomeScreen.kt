f1d2e10c48faa3df15ccf10fede9d028bdec7e5b
aminamehdiyeva <aminamehdi62@gmail.com>
2024-07-22 11:27:16 +0400

    .

---
+++ app/src/main/java/com/issuesolver/presentation/home/HomeScreen.kt
@@ -0,0 +1,34 @@
+package com.issuesolver.presentation.home
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.issuesolver.presentation.bottombar.AnimatedNavigationBar
+
+
+@Composable
+fun TestUI(
+//    navController: NavController,
+//    viewModel:  = hiltViewModel(),
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .statusBarsPadding()
+            .background(color = Color.Red)
+//
+    ) {
+
+    }
+
+}
+
+
+
+
