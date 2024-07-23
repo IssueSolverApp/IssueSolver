0c3b93cc59d2ae5d6da35f8c2d6590b0ba49ac96
aminamehdiyeva <aminamehdi62@gmail.com>
2024-07-23 19:19:17 +0400

    .

--- app/src/main/java/com/issuesolver/presentation/MainActivity.kt
+++ app/src/main/java/com/issuesolver/presentation/MainActivity.kt
@@ -20,8 +20,7 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             IssueSolverTheme {
-                Splash()
-//                RootNavigationGraph()
+                RootNavigationGraph()
             }
         }
     }
