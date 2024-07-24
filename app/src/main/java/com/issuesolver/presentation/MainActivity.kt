a7e71d04c806162f4debcac9115d359b3146c82b
YuriyQukunov <kukunovyura29@gmail.com>
2024-07-24 11:23:03 -0700

    fixMainActivity

--- app/src/main/java/com/issuesolver/presentation/MainActivity.kt
+++ app/src/main/java/com/issuesolver/presentation/MainActivity.kt
@@ -21,8 +21,7 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             IssueSolverTheme {
-                UserCard()
-                //RootNavigationGraph()
+                RootNavigationGraph()
             }
         }
     }
